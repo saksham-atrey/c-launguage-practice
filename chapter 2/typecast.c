5aeb6639a2f4566e813fddeab4d04869318c2caa
Saksham-Atrey <sakshamatrey1@gmail.com>
2026-08-30 20:01:20 +0530

    Add files via upload

---
+++ chapter 2/typecast.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    float m = 67.89;
+    n = (int) m;
+printf("%d", n);
+    return 0;
+}
---
+++ chapter 2/variables.c
@@ -0,0 +1,2 @@
+// %d is called format specifier
+// %d is for int, %f is for float, %c is for char
