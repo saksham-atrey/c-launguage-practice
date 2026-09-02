b93a114d18a4667a713768224dc3d23560b277f1
saksham-atrey <sakshamatrey1@gmail.com>
2026-09-02 17:46:33 +0530

    Create conditionals.c

---
+++ chapter 3/conditionals.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20;
+    if (age > 15){
+        printf("Your age is greater than 15\n");
+    };
+    if(age%5==0){
+        printf("Your age is divisible by 5");
+
+    };
+    return 0;
+
+}
