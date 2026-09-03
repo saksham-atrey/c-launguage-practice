78c1e0ad8d28fb2aeb2ce1a79b84d18431296859
saksham-atrey <sakshamatrey1@gmail.com>
2026-09-03 22:58:52 +0530

    Create while_loop.c

---
+++ chapter 4/while_loop.c
@@ -0,0 +1,11 @@
+//loop starts from initial value given to variable
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    while(i<4){
+        printf("yo\n");
+        i=i+1;
+    }
+    return 0;
+}
